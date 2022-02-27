3376328b51111c6661f3d90a63e3ef6ead37a63b
Jandres1420 <juan.pico-m@mail.escuelaing.edu.co>
2022-02-27 16:07:45 -0500

    Primer punto

---
+++ SpringBoot_REST_API_Blueprints_Part2/src/main/java/edu/eci/arsw/blueprints/Application.java
@@ -0,0 +1,55 @@
+package edu.eci.arsw.blueprints;
+
+import edu.eci.arsw.blueprints.filters.FilterA;
+import edu.eci.arsw.blueprints.filters.FilterB;
+import edu.eci.arsw.blueprints.filters.Filters;
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.model.Point;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Set;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ac = new 
+        BlueprintsServices services = ac.getBean(BlueprintsServices.class);
+        Point[] points = new Point[] { new Point(2, 2), new Point(3, 5), new Point(2, 2) };
+        Point[] points2 = new Point[] { new Point(2, 20), new Point(33, 5), new Point(22, 12) };
+        Blueprint mockBlueprint = new Blueprint("Descartes", "Discurso del metodo", points);
+        Blueprint book2 = new Blueprint("Descartes", "Meditaciones metafisicas");
+        Blueprint book3 = new Blueprint("Shopenhauer", "Die Welt als Wille und Vorstellung", points2);
+        book2.addPoint(new Point(31, 2));
+        book2.addPoint(new Point(31, 2));
+        try {
+            services.addNewBlueprint(mockBlueprint);
+            services.addNewBlueprint(book2);
+            services.addNewBlueprint(book3);
+            Set<Blueprint> blueprintSet = services.getBlueprintsByAuthor("Descartes");
+            Set<Blueprint> allBlueprints = services.getAllBlueprints();
+            Filters filterA = new FilterA();
+            Filters filterB = new FilterB();
+            filterA.filterAllBlueprints(blueprintSet);
+            filterB.filterAllBlueprints(allBlueprints);
+            System.out.println("filter A to Descartes blueprints");
+            for (Blueprint bp : blueprintSet) {
+                System.out.println(bp.toString());
+                for (Point p : bp.getPoints()) {
+                    System.out.println(p.toString());
+                }
+            }
+            System.out.println("filter B to every blueprint");
+            for (Blueprint bp : allBlueprints) {
+                System.out.println(bp.toString());
+                for (Point p : bp.getPoints()) {
+                    System.out.println(p.toString());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ SpringBoot_REST_API_Blueprints_Part2/src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.blueprints.persistence.impl;
+
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.model.Point;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
+import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author hcadavid
+ */
+@Controller
+public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
+
+    private final Map<Tuple<String,String>,Blueprint> blueprints=new HashMap<>();
+
+    public InMemoryBlueprintPersistence() {
+        //load stub data
+        Point[] pts=new Point[]{new Point(140, 140),new Point(115, 115)};
+        Blueprint bp=new Blueprint("_authorname_", "_bpname_ ",pts);
+        blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
+        
+    }    
+    
+    @Override
+    public void saveBlueprint(Blueprint bp) throws BlueprintPersistenceException {
+        if (blueprints.containsKey(new Tuple<>(bp.getAuthor(),bp.getName()))){
+            throw new BlueprintPersistenceException("The given blueprint already exists: "+bp);
+        }
+        else{
+            blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
+        }        
+    }
+
+    @Override
+    public Blueprint getBlueprint(String author, String bprintname) throws BlueprintNotFoundException {
+        if(blueprints.size() <= 1){
+            throw new BlueprintNotFoundException("there is no blueprint");
+        }
+        Blueprint blueprint = blueprints.get(new Tuple<>(author, bprintname));
+        if(blueprint == null){
+            throw new BlueprintNotFoundException("blueprint doesn't exist");
+        }
+        return blueprint;
+
+    }
+
+    @Override
+    public Set<Blueprint> getBlueprintsByAuthor(String author) throws BlueprintNotFoundException {
+        if(blueprints.size() <= 1){
+            throw new BlueprintNotFoundException("there is no blueprint");
+        }
+        Set<Blueprint> blueprintSet = new HashSet<>();
+        for(Map.Entry<Tuple<String,String>,Blueprint> entry : blueprints.entrySet()){
+            if (entry.getKey().getElem1().equals(author)){
+                blueprintSet.add(entry.getValue());
+            }
+
+        }
+        if(blueprintSet.isEmpty()){
+            throw new BlueprintNotFoundException("blueprints from this author doesn't exist");
+        }
+        return blueprintSet;
+    }
+
+    @Override
+    public Set<Blueprint> getAllBlueprints() throws BlueprintNotFoundException {
+        if(blueprints.size() <= 1){
+            throw new BlueprintNotFoundException("there is no blueprint");
+        }
+        return blueprints.values().stream().collect(Collectors.toSet());
+    }
+
+
+}
